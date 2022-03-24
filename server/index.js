c6bf2ecb320d93c52ea632a7c0c49319bb738938
Jorgelly Ventura <jorgelly87@gmail.com>
2022-03-24 15:46:59 -0400

    typo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const db = require('./db/database');
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const init = async () => {
   await db.sync({force: true});
