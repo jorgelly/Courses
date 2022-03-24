036e832419f4482b9568d144139c7721e57c4b90
Jorgelly Ventura <jorgelly87@gmail.com>
2022-03-24 15:11:53 -0400

    Added env.process.DATABASE_URL for heroku deployment

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,17 @@
 const Sequelize = require('sequelize')
 
+
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  };
+};
+
 const dbName = 'canoe-courses'
 console.log(`Opening database connection to ${dbName}`);
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`, {
   logging: false,
 });
 
