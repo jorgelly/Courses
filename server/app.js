8d9f122a831bb454f8368300870010b5a968e87e
Jorgelly Ventura <jorgelly87@gmail.com>
2022-03-24 16:57:47 -0400

    testing

--- server/app.js
+++ server/app.js
@@ -8,11 +8,12 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../public/index.html')));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', require('./api'))
 
-app.get('*', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
